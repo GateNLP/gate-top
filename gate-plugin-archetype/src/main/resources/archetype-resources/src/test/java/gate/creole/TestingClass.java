25552114d4d14b55316ce4e9f8fa4549e0de2f02
Johann Petrak <johann.petrak@gmail.com>
2017-11-18 16:04:10 +0000

    Add a few comments and add an example class for unit testing.

---
+++ gate-plugin-archetype/src/main/resources/archetype-resources/src/test/java/gate/creole/TestingClass.java
@@ -0,0 +1,21 @@
+package gate.creole;
+
+import gate.test.GappLoadingTestCase;
+
+/**
+ * Using this class automatically prepares GATE and the plugin for testing.
+ * 
+ * This class automatically initializes GATE and loads the plugin. 
+ * Any method in this class with the "@Test" annotation will then get
+ * run with the plugin already properly loaded.
+ * 
+ * NOTE: please move/copy this class to the correct package for your plugin!
+ 
+ */
+public class GappLoadingTest extends GappLoadingTestCase {
+
+  @Test
+  public void testSomething() {
+    // testing code goes here
+  }
+}
